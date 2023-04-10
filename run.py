a71b9bef5754a97c1a7f9cfc9857d0202619ca2a
arav-dhoot <65766560+arav-dhoot@users.noreply.github.com>
2023-04-10 19:50:29 +0530

    three code runs

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
 
     experiment_name = 'fpt'
 
-    experiment_params = dict(
+    frozen_params = dict(
         task='bit-xor',
         n=1000,                # ignored if not a bit task
         num_patterns=5,        # ignored if not a bit task
@@ -34,4 +34,66 @@ if __name__ == '__main__':
                       # orthogonal initialization of input layer
     )
 
-    experiment.run_experiment(experiment_name, experiment_params)
+    finetuned_params = dict(
+        task='bit-xor',
+        n=1000,                # ignored if not a bit task
+        num_patterns=5,        # ignored if not a bit task
+        # patch_size=50,       if the task is bit    
+        patch_size=4,
+
+        model_name='gpt2',
+        pretrained=True,       # if vit this is forced to true, if lstm this is forced to false
+
+        freeze_trans=True,     # if False, we don't check arguments other than in and out
+        freeze_in=False,
+        freeze_pos=False,
+        freeze_ln=False,
+        freeze_attn=True,
+        freeze_ff=True,
+        freeze_out=False,
+
+        training_type='finetune',
+
+        in_layer_sizes=None,   # not in paper, but can specify layer sizes for an MLP,
+        out_layer_sizes=None,  # ex. [32, 32] creates a 2-layer MLP with dimension 32
+
+        learning_rate=1e-3,
+        batch_size=4,
+        dropout=0.1,
+        orth_gain=1.41, 
+                      # orthogonal initialization of input layer
+    )
+
+    optimized_params = dict(
+        task='bit-xor',
+        n=1000,                # ignored if not a bit task
+        num_patterns=5,        # ignored if not a bit task
+        # patch_size=50,       if the task is bit    
+        patch_size=4,
+
+        model_name='gpt2',
+        pretrained=True,       # if vit this is forced to true, if lstm this is forced to false
+
+        freeze_trans=True,     # if False, we don't check arguments other than in and out
+        freeze_in=False,
+        freeze_pos=False,
+        freeze_ln=False,
+        freeze_attn=True,
+        freeze_ff=True,
+        freeze_out=False,
+
+        training_type='optimized',
+
+        in_layer_sizes=None,   # not in paper, but can specify layer sizes for an MLP,
+        out_layer_sizes=None,  # ex. [32, 32] creates a 2-layer MLP with dimension 32
+
+        learning_rate=1e-3,
+        batch_size=4,
+        dropout=0.1,
+        orth_gain=1.41, 
+                      # orthogonal initialization of input layer
+    )
+
+    experiment.run_experiment(experiment_name, frozen_params)
+    experiment.run_experiment(experiment_name, finetuned_params)
+    experiment.run_experiment(experiment_name, optimized_params)
