4a99b6e848dc0788311d449c88f50911ef8248fd
Arav Dhoot <aravdhoot@gmail.com>
2023-05-19 07:32:10 +0000

    minor code change

--- run.py
+++ run.py
@@ -124,8 +124,8 @@ if __name__ == '__main__':
                       # orthogonal initialization of input layer
     )
 
-    experiment.run_experiment(experiment_name, frozen_params)
     experiment.run_experiment(experiment_name, finetuned_params)
+    experiment.run_experiment(experiment_name, frozen_params)    
     experiment.run_experiment(experiment_name, optimized_params)
     experiment.run_experiment(experiment_name, snr_params)
     
